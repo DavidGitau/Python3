6862535d55e872e904422c5c1f31d2e19e7516f0
DavidGitau <86714248+DavidGitau@users.noreply.github.com>
2022-08-13 12:09:52 +0300

    Tuples complete

---
+++ Tutorial/08.Tuples/tuple.py
@@ -0,0 +1,9 @@
+#  Collecion which is ordered and unchangeable. Usec to group together related data
+
+student = ('David',21,'male')
+
+print(student.count('male'))
+print(student.index('David'))
+
+for x in student:
+    print(x)
