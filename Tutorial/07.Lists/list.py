0b8136459cf00d5cf8054d77699412ac8145c82d
DavidGitau <86714248+DavidGitau@users.noreply.github.com>
2022-08-13 11:35:40 +0300

    2D lists

--- Tutorial/07.Lists/list.py
+++ Tutorial/07.Lists/list.py
@@ -7,3 +7,14 @@ food.pop()                          # remove the last item
 food.insert(0,'cake')               # insert item in a given index
 food.sort()
 food.clear()                        # remove everything in the list
+
+for i in food: print(i)
+
+# 2D lists
+drinks = ['coffee','soda','tea']
+dinner = ['pizza','hamburger','hotdog']
+dessert = ['cake','ice cream']
+
+foods = [drinks,dinner,dessert]
+
+print(foods[1][2])
