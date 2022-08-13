cc0ec17ed1e8ccdf0cf91693e528a5dad35ac891
DavidGitau <86714248+DavidGitau@users.noreply.github.com>
2022-08-13 13:27:51 +0300

    Function Keyword Arguments

--- Tutorial/10.Functions/funcs.py
+++ Tutorial/10.Functions/funcs.py
@@ -10,3 +10,12 @@ def multiply(a,b):
     return a * b
 
 print(multiply(2,8))
+
+"""
+Keyword arguments 
+Preceeded by an identifier when we pass them to a function 
+The order of the arguments does not matter, unlike positional arguments
+Python knows the names of the arguments that the function receives
+
+"""
+hello(lname='John', fname='Jones')
