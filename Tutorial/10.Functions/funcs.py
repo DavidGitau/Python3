95e572574d50b92289da5d3787953b81b650dbe9
DavidGitau <86714248+DavidGitau@users.noreply.github.com>
2022-08-13 13:34:06 +0300

    Function Keyword Arguments

--- Tutorial/10.Functions/funcs.py
+++ Tutorial/10.Functions/funcs.py
@@ -19,3 +19,17 @@ Python knows the names of the arguments that the function receives
 
 """
 hello(lname='John', fname='Jones')
+
+
+"""
+*args
+parameter that will pack all arguments into a tuple
+useful so that a function can accept a varying amount of arguments
+"""
+def add(*args):
+    sum = 0
+    for i in args:
+        sum += i
+    return sum
+
+print(add(1,2,3,4,5))
