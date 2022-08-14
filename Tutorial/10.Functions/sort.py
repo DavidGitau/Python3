8b477a792ab9e8fbacd2baf6d493b827fddc6784
DavidGitau <86714248+DavidGitau@users.noreply.github.com>
2022-08-14 17:40:05 +0300

    Mapping lists and iterables

--- Tutorial/10.Functions/sort.py
+++ Tutorial/10.Functions/sort.py
@@ -35,4 +35,10 @@ store = [
     ('socks', 10.00)
 ]
 
-to_euros = lambda data: (data[0],({.2f}).format(data[1]*0.82))
+to_euros = lambda data: (data[0],'{:.2f}'.format(data[1]*0.82))
+to_dollars = lambda data: (data[0],'{:.2f}'.format(data[1]/0.82))
+
+store_euros = list(map(to_euros, store))
+store_dollars = list(map(to_dollars, store))
+
+for e in store_dollars: print(e)
